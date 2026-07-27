ec429898262e58d479a194d6cf637dcfa31cfab6
KevinEpn <kevin.asimbaya@epn.edu.ec>
2026-07-27 18:07:31 -0500

    Add command line arguments to exercise generator

--- create_exercise.py
+++ create_exercise.py
@@ -4,8 +4,19 @@ Creates the initial structure for a Python excersice.
 """
 
 # imports
+import argparse
 
 # constants
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parse command line arguments
+    """
+
+    parser = argparse.ArgumentParser(description="Create a new exercise structure")
+    parser.add_argument("exercise_id", type=str, help="The ID of the exercise to create")
+    parser.add_argument("exercise_name", type=str, help="The name of the exercise to create")
+
+    return parser.parse_args()
 
 
 # functions
@@ -14,7 +25,12 @@ def main() -> None:
     Appication entry point
     """
 
+    args = parse_arguments()
+
     print("Exercise Generator")
+    print()
+    print(f"Exercise ID: {args.exercise_id}")
+    print(f"Exercise Name: {args.exercise_name}")
 
 
 if __name__ == "__main__":
