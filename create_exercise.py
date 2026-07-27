132524c90175a8deab69ffe67a4733f314d8f0ef
KevinEpn <kevin.asimbaya@epn.edu.ec>
2026-07-27 17:33:09 -0500

    Create exercise generator structure

---
+++ create_exercise.py
@@ -0,0 +1,15 @@
+# Create_excercise.py
+"""
+Creates the initial structure for a Python excersice.
+"""
+
+# imports
+
+# constants
+
+# functions
+def main():
+    print("Exercise Generator")
+
+if __name__ == "__main__":
+    main()
